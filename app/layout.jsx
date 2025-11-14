bf58f5823913198e596f51678bc8bb4fd2974b73
Adam Cole <adam@gofastcrushgoals.com>
2025-11-14 04:15:55 -0500

    fix: remove manual head tag - Next.js metadata handles favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,10 +15,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
-      </head>
       <body>{children}</body>
     </html>
   );
